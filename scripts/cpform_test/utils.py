d47930614aefd0146bbba1fb32970edf6883207f
CangSpirit <cpcgskill@outlook.com>
2025-08-03 22:49:15 +0800

    test: ✅ 2024 积累更新，补充

---
+++ scripts/cpform_test/utils.py
@@ -0,0 +1,11 @@
+# -*-coding:utf-8 -*-
+"""
+:PROJECT_NAME: cpform
+:File: utils.py
+:Time: 2024/10/18 19:27
+:Author: 张隆鑫
+"""
+from __future__ import unicode_literals, print_function, division
+
+if False:
+    from typing import *
